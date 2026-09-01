37b43bedd41984937bc603f0c57329610215cacb
araguaa <willianperes@rede.ulbra.br>
2026-09-01 14:49:58 -0300

    feat: cria rota inicio

---
+++ app/inicio.tsx
@@ -0,0 +1,28 @@
+import {
+    SafeAreaView,
+    StyleSheet,
+    Text,
+} from "react-native";
+
+export default function Inicio() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Tela 02.1 - Início
+      </Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: "700",
+  },
+});
